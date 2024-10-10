e1d0202dae67e4d25c5e0b85d0690980a07385f0
VladyslavAbramov <abr.vlad1994@gmail.com>
2024-10-10 14:23:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+import datetime; should_i_take_a_break_and_chill_and_have_a_beer_or_coffee = lambda: 'No' if (datetime.datetime.today().day, datetime.datetime.today().strftime('%B')) == (29, 'February') else 'Yes'
